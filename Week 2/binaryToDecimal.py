eafa150fe70dba89f3bc8bd44d01cbfcbc448edb
ZUGRII <zu.georgiana@gmail.com>
2019-09-26 15:24:31 +0100

    Week 2 lab

---
+++ Week 2/binaryToDecimal.py
@@ -0,0 +1,11 @@
+#Convert Binary to Decimal
+#Author: Georgiana Zugravu
+#Date: 26th September 2019
+#Compiler: PyCharm
+
+import math
+
+binary_str = input("Enter a binary number: ")
+number = int(binary_str,2)
+
+print("The number is: ", number)
