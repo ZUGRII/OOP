71fbfc220f77171c8c3e26e3ad038efbf6893f30
ZUGRII <zu.georgiana@gmail.com>
2019-10-10 17:33:12 +0100

    week 3 lab

--- Week 3/ScrambleWords.py
+++ Week 3/ScrambleWords.py
@@ -1,7 +1,6 @@
 # Scramble Words program
 # eg: hello will become hlelo or hlleo
-# Author: Georgiana Zugravu
-# C18768301
+# Author: Georgiana Zugravu C18768301
 # Date: 9th October 2019
 
 import random
@@ -17,11 +16,18 @@ range_word=''
 
 # scramble each word
 for i in range(len(words)):
-    one_word = words[i]                                         # take each separate word
-        #if ord(i) < 65 or ord(i) > 122:
-    range_word = one_word[1:len(one_word)-1]                    # select the range that has to be scrambled
-    scramble = range_word[::-1]                                 # scramble the range of the word by reversing it
-    words[i]= one_word[0]+scramble+one_word[len(one_word)-1]    # concatenate the pieces
+    one_word = words[i]                                            # take each separate word
+
+    # if the last character of the word is not a letter, than create the proper range
+    if ord(one_word[len(one_word)-1]) < 65 or ord(one_word[len(one_word)-1]) > 122:
+       range_word = one_word[1:len(one_word)-2]                     # select the range that has to be scrambled
+       scramble = range_word[::-1]                                  # scramble the range of the word by reversing it
+       words[i] = one_word[0] + scramble + one_word[len(one_word) - 2: len(one_word)] # concatenate the pieces
+
+    else:
+        range_word = one_word[1:len(one_word)-1]                    # select the range that has to be scrambled
+        scramble = range_word[::-1]                                 # scramble the range of the word by reversing it
+        words[i]= one_word[0]+scramble+one_word[len(one_word)-1]    # concatenate the pieces
 
 i = 0
 text_str = ''
