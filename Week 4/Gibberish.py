6e6472e3925edf031193dc3520350a2bd38e2bbe
ZUGRII <zu.georgiana@gmail.com>
2019-10-11 17:24:19 +0100

    week 4 group project

--- Week 4/Gibberish.py
+++ Week 4/Gibberish.py
@@ -1,43 +1,73 @@
 # Gibberish program
-# Author:Georgiana Zugravu C18768301
+# Author:Georgiana Zugravu C18768301 +
 # Date: 10th October 2019
 
 import string
 import math
+
+def is_digit(syllable):
+    i = 0
+    check = False
+    for i in range(0, len(syllable)):
+
+        if ord(syllable[i]) < 65 or ord(syllable[i]) > 122 or ord(syllable) != 42 :
+            print("Syllable must only contain letters or a wildcard :'*'\nTry again!")
+            check = True
+
+    return check
+
+
+def asterix(syllable):
+    if ord(syllable[0]) == 42:
+        return True
+    else:
+        return False
+
+def add_syllable(new, syllable):
+    new = new + syllable
+    return new
+
 
 print("This is Gibberish game\nInsert 2 sets of characters and an word\n"
       "You will receive the word translated in Gibberish language")
 play='yes'
 
 while play=='yes' or play=='y':
-    first = input("Enter the first Gibberish syllable (add * for the vowel substitute): ")
-    second = input("Enter the second Gibberish syllable (* for vowel substitute):")
+    check = True
+    while check :
+        first = input("Enter the first Gibberish syllable (add * for the vowel substitute): ")
+        check = is_digit(first)
+    check = True
+    while check:
+        second = input("Enter the second Gibberish syllable (* for vowel substitute):")
+        check = is_digit(second)
+
     word = input("Please enter a word you want to translate: ")
     i = 0
     vowels = "aeiouAEIOU"
-    syllable=1
-    for i in word :
-        if i in vowels :
-            print("yeeeesss")
+    syllable = 1
+    new_word = ''
+
+    for i in range(0, len(word)):
+        if word[i] in vowels :
+
             if syllable == 1:
-                word = word[0:word.find(i)] + first + word[word.find(i)+1: len(word)-1]
+
+                if asterix(first):
+                    first = word[i] + first[1: len(first)]
+                new_word = add_syllable(new_word, first)
+
                 syllable = syllable + 1
             else:
-                word = word[0:word.find(i)] + second + word[word.find(i) + 1: len(word) - 1]
+                if word[i-1] not in vowels :
+                    if asterix(second):
+                        second = word[i] + second[1:len(second)]
+                    new_word = add_syllable(new_word, second)
+
+
+        new_word = new_word + word[i]
 
 
 
-    print("Your final word: ", word)
+    print("Your final word: ", new_word)
     play = input("Play again? (yes/no)")
-
-#def add_syllable()
-
-def  is_digit(syllable):
-    return syllable.isdigit()
-
-
-def asterix(syllable):
-    if ord(syllable[0]==42):
-        return True
-    else:
-        return False
