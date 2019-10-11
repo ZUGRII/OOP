1d8927ae74683c04b3e3637452380e0a0ffe92ef
ZUGRII <zu.georgiana@gmail.com>
2019-10-11 19:16:48 +0100

    week 4 group project

--- Week 4/Gibberish.py
+++ Week 4/Gibberish.py
@@ -1,73 +1,90 @@
 # Gibberish program
-# Author:Georgiana Zugravu C18768301 +
+# Authors: Georgiana Zugravu C18768301 + Yecenia Gonzalez C18738429
 # Date: 10th October 2019
+# This is the gibberish game, this program allows a user to insert two "gibberish" (eg.'pi','ru'se') that will be use in
+# a word, also inserted by the user, to translate it from english to "gibberish". The user can use a wildcard (*) that
+# can only appear at the beginning of the syllable.
 
-import string
-import math
 
+# Generate method 'is_digit'
+# This will check if the input 'gibberish'is an alphabet letter
 def is_digit(syllable):
     i = 0
     check = False
+    alfabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ*"
     for i in range(0, len(syllable)):
 
-        if ord(syllable[i]) < 65 or ord(syllable[i]) > 122 or ord(syllable) != 42 :
+        if syllable[i] not in alfabet:
             print("Syllable must only contain letters or a wildcard :'*'\nTry again!")
             check = True
 
     return check
 
 
+# Generate method 'asterix'
+# This method will check if the inputted 'gibberish' is the wildcard character (*)
 def asterix(syllable):
     if ord(syllable[0]) == 42:
         return True
     else:
         return False
 
+
+# Generate method 'add_syllable'
+# This method will contain the translated word
 def add_syllable(new, syllable):
     new = new + syllable
     return new
 
+# Game intro
+print("\n\n\nThis is Gibberish game!\nYou will need to insert 2 sets of characters and a word\n"
+      "You will get the word translated in Gibberish language !\n"
+      "Let's play!")
+play = 'yes'
 
-print("This is Gibberish game\nInsert 2 sets of characters and an word\n"
-      "You will receive the word translated in Gibberish language")
-play='yes'
-
-while play=='yes' or play=='y':
+# Prompt user for input
+while play == 'yes' or play == 'y':
     check = True
-    while check :
-        first = input("Enter the first Gibberish syllable (add * for the vowel substitute): ")
+    while check:
+        first = input("Please enter the first Gibberish syllable (add * for the vowel substitute): ")
         check = is_digit(first)
     check = True
     while check:
-        second = input("Enter the second Gibberish syllable (* for vowel substitute):")
+        second = input("Please enter the second Gibberish syllable (* for vowel substitute):")
         check = is_digit(second)
 
+# Initialize variables
     word = input("Please enter a word you want to translate: ")
     i = 0
     vowels = "aeiouAEIOU"
     syllable = 1
     new_word = ''
 
-    for i in range(0, len(word)):
-        if word[i] in vowels :
-
-            if syllable == 1:
-
-                if asterix(first):
+# word check
+    for i in range(0, len(word)):  # for loop to check each letter of the word
+        if word[i] in vowels:  # checks if the letter at index position (i) is a vowel
+            if syllable == 1:  # checks the first vowel
+                if asterix(first):  # stores first vowel to use as the wildcard
                     first = word[i] + first[1: len(first)]
-                new_word = add_syllable(new_word, first)
 
-                syllable = syllable + 1
+                new_word = add_syllable(new_word, first)
+                syllable = syllable + 1 # change value os 'syllable' to stop the if statement
+
             else:
-                if word[i-1] not in vowels :
-                    if asterix(second):
+                if word[i - 1] not in vowels:  # if previous letter is not a vowel
+                    if asterix(second):  # stores second vowel to use as the wildcard
                         second = word[i] + second[1:len(second)]
                     new_word = add_syllable(new_word, second)
 
+        new_word = new_word + word[i] # add each letter of the input word to the new word
 
-        new_word = new_word + word[i]
+    print("Your final word is : ", new_word)  # Print out resulting word
+    check_play = True
+    while check_play :
+        play = input("Play again? (yes/no)")  # Ask user to play again
+        if play == "yes" or play == "no" or play == "n" or play == "y" :  # check the input
+            check_play = False
+        else :
+            print("Please enter y to continue or n to quit: ")
 
-
-
-    print("Your final word: ", new_word)
-    play = input("Play again? (yes/no)")
+print("Thank you for playing!")
