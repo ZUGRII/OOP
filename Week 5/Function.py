5bfc8916dc26aa88ab5d0c3e79b4d6e2e01c0aee
ZUGRII <zu.georgiana@gmail.com>
2019-10-17 15:28:16 +0100

    week 5 lab

--- Week 5/Function.py
+++ Week 5/Function.py
@@ -5,7 +5,19 @@
 
 
 def gcd(bigger, smaller) :
-    return bigger % smaller
+    if(bigger> smaller):
+        if (bigger % smaller == 0):
+            return smaller
+        else:
+            return bigger % smaller
+    else:
+        if(smaller % bigger == 0):
+            return bigger
+        else:
+            return smaller % bigger
+def lcm(first, second):
+    great = gcd(first, second)
+    return (first * second ) / gcd(first, second)
 
 
 
@@ -16,3 +28,8 @@ smaller = int(input("Please enter the smaller number: "))
 great =  gcd(bigger, smaller)
 print("THe greatest common divisor is: ", great)
 
+print("\n\n\n")
+first = int(input("Please enter the first number: "))
+second = int(input("Please enter the second number: "))
+least = lcm(first, second)
+print("The least common multiple is: ", least)
