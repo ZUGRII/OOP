ed2d01d5c3a50571379b7889c8e84fbb9e8844b7
ZUGRII <zu.georgiana@gmail.com>
2019-10-23 20:16:45 +0100

    week 5 lab

--- Week 5/Function.py
+++ Week 5/Function.py
@@ -13,9 +13,25 @@ def lcm(first, second):
     return (first * second) / gcd(first, second)
 
 
-#def addFrac(frac1, frac2):
+def addFrac(frac1, frac2):
+    up = (int(frac1[0]) * lcm(int(frac1[2]), int(frac2[2])) / int(frac1[2])) + \
+         (int(frac2[0]) *  lcm(int(frac1[2]), int(frac2[2])) /int(frac2[2]))
+    frac3 =(up, lcm(int(frac1[2]), int(frac2[2])))
+    return frac3
 
 
+def subFrac(frac1,frac2):
+    up = (int(frac1[0]) * lcm(int(frac1[2]), int(frac2[2])) / int(frac1[2])) - (
+                int(frac2[0]) * lcm(int(frac1[2]), int(frac2[2])) / int(frac2[2]))
+    frac3 = (up, lcm(int(frac1[2]), int(frac2[2])))
+    return frac3
+
+
+def reduce(frac):
+    print(frac[0])
+    print(frac[2])
+    frac = (int(frac[0]) / gcd(int(frac[0]), int(frac[2])) , gcd(int(frac[0]), int(frac[2])))
+    return frac
 
 
 # main
@@ -32,9 +48,24 @@ least = lcm(first, second)
 print("The least common multiple is: ", least)
 
 print("\n\n\n")
+fraction_one = ()
+fraction_two = ()
 fraction_one = input("Please enter the fraction numbers followed by comma :")
-frac1 = fraction_one.split(',')
 fraction_two = input("Please enter the fraction numbers followed by comma :")
-frac2 = fraction_two.split(',')
+print(fraction_one)
+print(fraction_two)
+add = ()
+add = addFrac(fraction_one, fraction_two)
+print("The sum of the fractions is: ",add)
 
-addFrac(frac1, frac2)
+sub = ()
+sub = subFrac(fraction_one, fraction_two)
+print("\nThe difference is: ", sub)
+
+print("\n\n\n")
+fraction = ()
+fraction = input("Please enter the fraction numbers followed by comma :")
+r = ()
+r = reduce(fraction)
+print("Fraction reduce is: ", r)
+
