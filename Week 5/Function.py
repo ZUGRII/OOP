6db7eb4ecffddc1c6ae5bf07cc46a16eccbc483c
ZUGRII <zu.georgiana@gmail.com>
2019-10-24 10:57:35 +0100

    week 5 lab

--- Week 5/Function.py
+++ Week 5/Function.py
@@ -14,23 +14,23 @@ def lcm(first, second):
 
 
 def addFrac(frac1, frac2):
-    up = (int(frac1[0]) * lcm(int(frac1[2]), int(frac2[2])) / int(frac1[2])) + \
-         (int(frac2[0]) *  lcm(int(frac1[2]), int(frac2[2])) /int(frac2[2]))
-    frac3 =(up, lcm(int(frac1[2]), int(frac2[2])))
+    up = (int(frac1[0]) * lcm(int(frac1[1]), int(frac2[1])) / int(frac1[1])) + \
+         (int(frac2[0]) *  lcm(int(frac1[1]), int(frac2[1])) /int(frac2[1]))
+    frac3 =(up, lcm(int(frac1[1]), int(frac2[1])))
     return frac3
 
 
 def subFrac(frac1,frac2):
-    up = (int(frac1[0]) * lcm(int(frac1[2]), int(frac2[2])) / int(frac1[2])) - (
-                int(frac2[0]) * lcm(int(frac1[2]), int(frac2[2])) / int(frac2[2]))
-    frac3 = (up, lcm(int(frac1[2]), int(frac2[2])))
+    up = (int(frac1[0]) * lcm(int(frac1[1]), int(frac2[1])) / int(frac1[1])) - (
+                int(frac2[0]) * lcm(int(frac1[1]), int(frac2[1])) / int(frac2[1]))
+    frac3 = (up, lcm(int(frac1[1]), int(frac2[1])))
     return frac3
 
 
 def reduce(frac):
     print(frac[0])
-    print(frac[2])
-    frac = (int(frac[0]) / gcd(int(frac[0]), int(frac[2])) , gcd(int(frac[0]), int(frac[2])))
+    print(frac[1])
+    frac = (int(frac[0]) / gcd(int(frac[0]), int(frac[1])) , int(frac[1]) / gcd(int(frac[0]), int(frac[1])))
     return frac
 
 
@@ -50,8 +50,8 @@ print("The least common multiple is: ", least)
 print("\n\n\n")
 fraction_one = ()
 fraction_two = ()
-fraction_one = input("Please enter the fraction numbers followed by comma :")
-fraction_two = input("Please enter the fraction numbers followed by comma :")
+fraction_one = tuple([eval(x) for x in input("Please enter the fraction numbers followed by comma :").split(',')])
+fraction_two = tuple([eval(x) for x in input("Please enter the fraction numbers followed by comma :").split(',')])
 print(fraction_one)
 print(fraction_two)
 add = ()
@@ -64,7 +64,7 @@ print("\nThe difference is: ", sub)
 
 print("\n\n\n")
 fraction = ()
-fraction = input("Please enter the fraction numbers followed by comma :")
+fraction = tuple([eval(x) for x in input("Please enter the fraction numbers followed by comma :").split(',')])
 r = ()
 r = reduce(fraction)
 print("Fraction reduce is: ", r)
