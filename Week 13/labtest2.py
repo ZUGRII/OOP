8e3db952e808dea78bd6bb86ef930b38d62e3fda
ZUGRII <zu.georgiana@gmail.com>
2019-12-12 14:51:21 +0000

    labtest2

---
+++ Week 13/labtest2.py
@@ -0,0 +1,73 @@
+# LAB TEST 2
+# Authors: Georgiana Zugravu C18768301
+# Date: 12th of December 2019
+# Compiler: PyCharm
+
+class Vector3D (object):
+    def __init__(self, x=0, y=0, z=0):
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+
+    def __add__(self, param):
+        newx = self.x + param.x
+        newy = self.y + param.y
+        newz = self.z + param.z
+
+        output = str.format('{:0.6f}, {:0.6f}, {:0.6f}', newx, newy, newz)
+        return output
+
+    def __sub__(self, param):
+        newx = self.x + (param.x*(-1))
+        newy = self.y + (param.y*(-1))
+        newz = self.z + (param.z*(-1))
+
+        output = str.format('{:0.6f}, {:0.6f}, {:0.6f}', newx, newy, newz)
+        return output
+        #return self.__sub__()
+
+    def __mul__(self, param):
+        if type(param) == int:
+            newx = self.x * param
+            newy = self.y * param
+            newz = self.z * param
+
+            output = str.format('{:0.6f}, {:0.6f}, {:0.6f}', newx, newy, newz)
+            return output
+
+        else:
+            new = self.x*param.x + self.y*param.y + self.z*param.z
+            return new
+
+
+    def __str__(self):
+        output = str.format('{:0.6f}, {:0.6f}, {:0.6f}',self.x, self.y, self.z )
+        return output
+
+
+
+
+# sample call to test the class
+
+v1 = Vector3D(1,2,3)
+v2 = Vector3D(5,5,5)
+
+print("Printing v1")
+print("v1=", v1)
+
+print("Printing v2")
+print("v2=", v2)
+
+v3 = v1 + v2
+print("Printing addition")
+print("v1+v2 =", v3)
+
+v4 = v1 - v2
+print("Printing subtraction")
+print("v1-v2=", v4)
+
+print("Printing dot product")
+v5 = v1*v2
+print("v1*v2=", v5)
+
+print("Printing integer multiplication")
