18b054917f6e715b22cc92b1ded29ee4dcd72358
ZUGRII <zu.georgiana@gmail.com>
2019-12-12 15:24:43 +0000

    labtest2

--- Week 13/labtest2.py
+++ Week 13/labtest2.py
@@ -11,9 +11,7 @@ class Vector3D (object):
     def __init__(self, x=0, y=0, z=0):
         """Default constructor, verify the type of the coordinates"""
 
-        if type(x) == int or type(x) == float:
-            self.x = float(x)
-
+        self.x = float(x)
         self.y = float(y)
         self.z = float(z)
 
@@ -23,7 +21,7 @@ class Vector3D (object):
         newy = self.y + param.y
         newz = self.z + param.z
 
-        output = str.format('{:0.6f}, {:0.6f}, {:0.6f}', newx, newy, newz)
+        output = str.format('({:0.6f}, {:0.6f}, {:0.6f})', newx, newy, newz)
         return output
 
     def __sub__(self, param):
@@ -32,23 +30,21 @@ class Vector3D (object):
         newy = self.y + (param.y*(-1))
         newz = self.z + (param.z*(-1))
 
-        output = str.format('{:0.6f}, {:0.6f}, {:0.6f}', newx, newy, newz)
+        output = str.format('({:0.6f}, {:0.6f}, {:0.6f})', newx, newy, newz)
         return output
-        #return self.__sub__()
 
     def __mul__(self, param):
         """Metod which calculate the multiplication of
                 1. a vector by an integer
                 2. two vectors"""
-        if type(param) == int:
+        if type(param) == int or type(param) == float:
             newx = self.x * param
             newy = self.y * param
             newz = self.z * param
 
-            output = str.format('{:0.6f}, {:0.6f}, {:0.6f}', newx, newy, newz)
+            output = str.format('({:0.6f}, {:0.6f}, {:0.6f})', newx, newy, newz)
             return output
-
-        else:
+        elif type(param) == Vector3D:
             new = self.x*param.x + self.y*param.y + self.z*param.z
             return new
 
@@ -59,7 +55,7 @@ class Vector3D (object):
 
     def __str__(self):
         """String method, returns the coordinates of the vector"""
-        output = str.format('{:0.6f}, {:0.6f}, {:0.6f}',self.x, self.y, self.z )
+        output = str.format('({:0.6f}, {:0.6f}, {:0.6f})',self.x, self.y, self.z )
         return output
 
 
@@ -70,26 +66,30 @@ class Vector3D (object):
 v1 = Vector3D(1,2,3)
 v2 = Vector3D(5,5,5)
 
-print("Printing v1")
+print("\nPrinting v1")
 print("v1=", v1)
 
-print("Printing v2")
+print("\nPrinting v2")
 print("v2=", v2)
 
 v3 = v1 + v2
-print("Printing addition")
+print("\nPrinting addition")
 print("v1+v2 =", v3)
 
 v4 = v1 - v2
-print("Printing subtraction")
+print("\nPrinting subtraction")
 print("v1-v2=", v4)
 
-print("Printing dot product")
 v5 = v1*v2
+print("\nPrinting dot product")
 print("v1*v2=", v5)
 
-print("Printing integer multiplication")
 v6 = v1*2
+print("\nPrinting integer multiplication")
 print("v1*2=", v6)
 
-print("v1 magnitude is", v1.magnitude())
+v7 = v1*2.5
+print("\nPrinting integer multiplication")
+print("v1*2.5=", v7)
+
+print("\nv1 magnitude is", v1.magnitude())
